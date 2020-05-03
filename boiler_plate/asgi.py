f455b99a1db344c41a31ee902ff2c36d2a5b58fe
Tirso Rodriguez <tirso.rod@gmail.com>
2020-05-03 13:38:01 +0100

    Added first files for django

---
+++ boiler_plate/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for boiler_plate project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boiler_plate.settings')
+
+application = get_asgi_application()
